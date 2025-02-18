332607735a249d01021984d01a5525a296529bea
Aadithya-Git <aadithyaks22@gmail.com>
2025-02-18 10:59:18 +0530

    Initial Commit

---
+++ CryptoSphere/CryptoSphere/Aadithya/LoginPage/CS_LoginPage.swift
@@ -0,0 +1,17 @@
+//
+//  CS_LoginPageApp.swift
+//  CS_LoginPage
+//
+//  Created by Aadithya K S on 13/02/25.
+//
+
+import SwiftUI
+
+@main
+struct CS_LoginPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
